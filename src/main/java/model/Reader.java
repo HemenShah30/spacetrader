1ae26053b9f3317d2dab191bb26ada3c4863265c
Jack Croft <jackcroft2@yahoo.com>
2014-09-20 22:42:44 -0400

    Got semi-lazy and moved the PlanetNames file to where it could easily be used by Eclipse and the JAR, so that the JAR works perfectly now. Made relevant changes to the Reader and Universe classes to respect this change

--- src/main/java/model/Reader.java
+++ src/main/java/model/Reader.java
@@ -1,7 +1,6 @@
 package model;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,16 +22,14 @@ public class Reader {
 	 */
 	public List<String> readPlanetNames(String fileName) {
 		List<String> planetNames = new ArrayList<String>();
-		File planetFile = new File(fileName);
-		try {
-			Scanner reader = new Scanner(planetFile);
-			reader.useDelimiter("\n");
-			while (reader.hasNext())
-				planetNames.add(reader.next());
-			reader.close();
-		} catch (IOException ie) {
-			ie.printStackTrace();
-		}
+		InputStream is = ClassLoader
+				.getSystemResourceAsStream("model/PlanetNames.txt");
+
+		Scanner reader = new Scanner(is);
+		reader.useDelimiter("\n");
+		while (reader.hasNext())
+			planetNames.add(reader.next());
+		reader.close();
 
 		return planetNames;
 	}
