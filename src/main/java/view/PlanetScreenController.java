88d435cbc2979f310df5ee583ad504df1fd1d29b
Hemen Shah <hemenshah30@gatech.edu>
2014-09-22 18:30:42 -0400

    Seperating trade from planet. DO NOT make changes to TradeScreen2.fxml

--- src/main/java/view/PlanetScreenController.java
+++ src/main/java/view/PlanetScreenController.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 /**
  * Controller for the planet screen window
  * 
@@ -7,5 +10,24 @@ package view;
  *
  */
 public class PlanetScreenController {
-
+    @FXML
+    private Label cargoMedicine;
+    @FXML
+    private Label cargoNarcotics;
+    @FXML
+    private Label cargoWater;
+    @FXML
+    private Label cargoMachines;
+    @FXML
+    private Label cargoRobots;
+    @FXML
+    private Label cargoFirearms;
+    @FXML
+    private Label cargoGames;
+    @FXML
+    private Label cargoFood;
+    @FXML
+    private Label cargoOre;
+    @FXML
+    private Label cargoFur;
 }
