cb5f6a1e169aac9b932c3a2dccc7e87826239c30
Larry He <larry.he@gatech.edu>
2014-09-27 19:48:09 -0400

    Bugfix for Buy Max wiping out player inventory

--- src/main/java/model/Ship.java
+++ src/main/java/model/Ship.java
@@ -146,7 +146,8 @@ public class Ship {
 	 *            The number of this good being added
 	 */
 	public void addToCargo(GoodType goodType, Integer quantity) {
-		cargo.put(goodType, quantity);
+		Integer temp = cargo.get(goodType);
+		cargo.put(goodType, temp + quantity);
 		currCargo += quantity;
 	}
 
