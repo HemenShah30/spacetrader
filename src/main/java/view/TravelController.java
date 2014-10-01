86ea2f6cebec7eb54468b6a4fc75c39b140ff227
Hemen Shah <hemenshah30@gatech.edu>
2014-10-01 12:32:44 -0400

    TravelScreen and controller created. Refer to https://github.com/HemenShah30/MapTest to see basics on how to use

---
+++ src/main/java/view/TravelController.java
@@ -0,0 +1,51 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+
+public class TravelController {
+
+	@FXML
+	private Canvas miniMap;
+
+	@FXML
+	private Button backBtn;
+
+	@FXML
+	private Canvas localMap;
+
+	@FXML
+	private Button goBtn;
+
+	@FXML
+	private Button nextBtn;
+
+	@FXML
+	void showNextPlanet(ActionEvent event) {
+
+	}
+
+	@FXML
+	void selectRegionOfSpace(ActionEvent event) {
+
+	}
+
+	@FXML
+	void selectPlanet(ActionEvent event) {
+
+	}
+
+	@FXML
+	void backToPlanetScreen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void goToSelectedPlanet(ActionEvent event) {
+
+	}
+
+}
+
