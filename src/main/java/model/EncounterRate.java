9c5d54aa14045d7490ccad1a6ca904a225f9497e
Larry He <larry.he@gatech.edu>
2014-10-01 12:09:11 -0400

    Created EncounterRate enum to be used by pirates and police

---
+++ src/main/java/model/EncounterRate.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Enum representing the number of pirates/police you can expect to encounter
+ * 
+ * @author Larry He
+ * 
+ */
+public enum EncounterRate {
+	NONE, FEW, SOME, MANY, SWARMS
+}
