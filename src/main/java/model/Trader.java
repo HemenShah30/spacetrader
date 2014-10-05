db980ffdfc2bdee3f8659aa6a26bd09583524dc0
Larry He <larry.he@gatech.edu>
2014-10-04 21:16:38 -0400

    Traders now have a price and quantity for the good they're interested in.

--- src/main/java/model/Trader.java
+++ src/main/java/model/Trader.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 /**
  * Abstract class representing Traders.
  * 
@@ -21,7 +23,9 @@ public class Trader extends NPC {
 		setInvestor(investor);
 		setShip(ship);
 		generateIsSelling();
-		
+		generateGood();
+		generatePrice();
+		generateQuantity();
 	}
 	
 	private void generateIsSelling() {
@@ -33,15 +37,18 @@ public class Trader extends NPC {
 	}
 	
 	private void generateGood() {
+		//not sure if this is the best way to do this. please let me know if you can think of a better way
 		this.goodOfInterest = goodOfInterest.getRandomGoodType();
 	}
 	
 	private void generatePrice() {
-		
+		Random random = new Random();
+		this.price = random.nextInt(goodOfInterest.getMaxTraderPrice()
+				- goodOfInterest.getMinTraderPrice() + 1) + goodOfInterest.getMinTraderPrice();
 	}
 	
 	private void generateQuantity() {
-		
+		this.quantity = (int) (10 * Math.random() + 5);
 	}
 	
 	public boolean getIsSelling() {
@@ -52,5 +59,11 @@ public class Trader extends NPC {
 		return goodOfInterest;
 	}
 	
+	public int getPrice() {
+		return price;
+	}
 	
+	public int getQuantity() {
+		return quantity;
+	}
 }
