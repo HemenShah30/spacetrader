282aebb48ceabd73dc3a681f50de0660fe0f0c07
Jack Croft <jackcroft2@yahoo.com>
2014-10-08 19:34:22 -0400

    Database layout

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -38,11 +38,40 @@ public class Database {
 	
 
 	//Things to save:
-	//Planet - TechLevel, SpecialResource, Government, Location, Condition, PoliceEncounterRate, PirateEnconuterRate, Marketplace
-	//Marketplace - prices, quantities, planet, radius, color
+	//Planet - TechLevel, SpecialResource, Government, Location, Condition, PoliceEncounterRate, PirateEnconuterRate, Marketplace, radius, color
+	//Marketplace - prices, quantities, planet
 	//Player - name, pilotSkill, fighterSkill, traderSkill, engineerSkill, investorSkill, traderRep, policeRep, pirateRep, credits, planet, Ship
 	//Ship - shipType, currHP, fuel, cargoSize, cargo(GoodType/quantity), weapons, shields, upgrades, mercenaries, insurance
 	//Mercenary - dailyCost, fighterSkill, pilotSkill, engineerSkill
+	//Bank - interestRate, outstandingDebt
+	//StockExchange - allStocks, allBonds, playerStocks, playerBonds
+	//Stock: price, company
+	//Bond: price, interestRate
+	
+	//Tables:
+	//Planet - PlanetId, TechLevelId, SpecialResourceId, GovernemntId, LocationX, LocationY, ConditionId, PoliceEncounterRateId, PirateEncounterRateId, MarketplaceId
+	//Government - GovernmentId, Name
+	//TechLevel - TechLevelId, Name, Value
+	//SpecialResource - SpecialResourceId, Name
+	//Government - GovernmentId, Name
+	//Condition - ConditionId, Name
+	//EncounterRate - EncounterRateId, Name
+	//GoodType - GoodTypeId, Name, MinTechLevelToProduct, MinTechLevelToUse, BiggestProducer, BasePrice, PriceIncreasePerTechLevel, PriceVariance, NegativeCondition, CheapSpecialResource, ExpensiveSpecialResource, MinTraderPrice, MaxTraderPrice, BaseQuantity, QuantityIncreasePerTechLevel
+	//Marketplace - MarketplaceId, PlanetId
+	//MarketplaceGoods - MarketplaceId, GoodTypeId, GoodTypeQuantity, GoodTypePrice
+	//MarketplacePrice
+	//Player - PlayerId, Name, PilotSkill, FighterSkill, TraderSkill, EngineerSkill, InvestorSkill, TraderReputation, PoliceReputation, PirateReputation, Credits, ShipId, PlanetId
+	//Ship - ShipId, ShipTypeId, CurrentHullPoints, Fuel, CargoSize, Insurance
+	//ShipWeapons - ShipId, WeaponId
+	//ShipShields - ShipId, ShieldId
+	//ShipUpgrades - ShipId, UpgradeId
+	//ShipMercenaries - ShipId, MercenaryId
+	//ShipCargo - ShipId, GoodTypeId, GoodTypeQuantity
+	//Mercenary - MercenaryId, DailyCost, FighterSkill, PilotSkill, EngineerSkill
+	//Bank - BankId, PlayerId, InterestRate, OutstandingPlayerDebt
+	//StockExchange - StockExchangeId, PlayerId
+	//Stock - StockId, StockExchangeId, CompanyId, Value
+	//Bond - BondId, StockExchangeId, InterestRate, Value
 	public void saveGame(Universe universe, Player p) {
 		
 	}
