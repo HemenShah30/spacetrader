b40b3aa96d0d335a9a6b5a8b027d2dc06d745f54
Caroline Zhang <czhang363@gatech.edu>
2014-10-08 14:27:12 -0400

    **DISCLAIMER: This push was mostly for Caroline because she may or may not have access to her laptop in a few hours Created barebones FightEngine class(does absolutely nothing at the moment); Created Gadget interface(currently only holds irrelevant information)

---
+++ src/main/java/model/Gadget.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * Interface encompassing the various gadgets
+ * 
+ * @author Caroline Zhang
+ *
+ */
+public interface Gadget {
+	
+//	FIVEEXTRACARGO(2000, 4),
+//	NAVIGATINGSYSTEM(25000, 5),
+//	AUTOREPAIRSYSTEM(25000, 5),
+//	TARGETINGSYSTEM(25000, 5),
+//	CLOAKINGDEVICE(40000, 6);
+//
+//  private int price;
+//	private int minTechLevel;
+//
+//	/**
+//	 * Gets the minimum tech level for the gadget
+//	 * 
+//	 * @return The minimum tech level for the gadget
+//	 */
+//	public int getMinTechLevel() {
+//		return minTechLevel;
+//	}
+//
+//	/**
+//	 * Returns the price of the gadget
+//	 * 
+//	 * @return The price of the gadget
+//	 */
+//	public int getPrice() {
+//		return price;
+//	}
+
+}
