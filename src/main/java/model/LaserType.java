a226b91cf06cd30aed9014807e2685f88915d3f9
Caroline Zhang <czhang363@gatech.edu>
2014-10-08 12:48:17 -0400

    Created basic enums for Lasers and Shields; made minor changes to ShipType

---
+++ src/main/java/model/LaserType.java
@@ -0,0 +1,64 @@
+package model;
+
+/**
+ * Enum representing the various lasers
+ * 
+ * @author Caroline Zhang
+ *
+ */
+public enum LaserType {
+
+	PULSELASER(20, 5000, 4),
+	BEAMLASER(35, 15000, 5),
+	MILITARYLASER(60, 35000, 6);
+
+	private int baseDamage;
+	private int minTechLevel;
+	private int price;
+
+
+	/**
+	 * Constructor for the Laser enums
+	 * 
+	 * @param d
+	 *            The damage dealt by the laser
+	 * @param p
+	 *            The price of the laser
+	 * @param mtl
+	 *            The minimum tech level required for a laser
+	 */
+	private LaserType(int bd, int p, int mtl) {
+		baseDamage = bd;
+		price = p;
+		minTechLevel = mtl;
+	}
+
+	/**
+	 * Returns the damage done by the laser
+	 * 
+	 * @return The damage done by the laser
+	 */
+	public int getBaseDamage() {
+		return baseDamage;
+	}
+
+
+	/**
+	 * Gets the minimum tech level for the laser
+	 * 
+	 * @return The minimum tech level for the laser
+	 */
+	public int getMinTechLevel() {
+		return minTechLevel;
+	}
+
+	/**
+	 * Returns the price of the laser
+	 * 
+	 * @return The price of the laser
+	 */
+	public int getPrice() {
+		return price;
+	}
+
+}
