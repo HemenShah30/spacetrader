30ff7ac9cfaf61946b96f9841154b11bb12716a2
Hemen Shah <hemenshah30@gatech.edu>
2014-10-25 23:32:39 -0400

    Ship sale side of shipyard ui + skeleton controller

---
+++ src/main/java/view/ShipyardController.java
@@ -0,0 +1,90 @@
+package view;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class ShipyardController {
+
+	@FXML
+	private Label playerShipNameLbl;
+
+	@FXML
+	private Label shipyardShipSheildSlotsLbl;
+
+	@FXML
+	private Label playerNameLbl;
+
+	@FXML
+	private Label shipyardShipNameLbl;
+
+	@FXML
+	private Label planetResourceLbl;
+
+	@FXML
+	private Label shipyardShipHPLbl;
+
+	@FXML
+	private Label shipyardShipGadgetSlotsLbl;
+
+	@FXML
+	private Label planetNameLbl;
+
+	@FXML
+	private Label planetTechLevelLbl;
+
+	@FXML
+	private Label playerShipFuelLbl;
+
+	@FXML
+	private Button buyShipBtn;
+
+	@FXML
+	private Label PlayerShipSheildSlotsLbl;
+
+	@FXML
+	private Label playerShipGadgetSlotsLbl;
+
+	@FXML
+	private Label shipyardShipCargoSizeLbl;
+
+	@FXML
+	private Label playerCreditsLbl;
+
+	@FXML
+	private Label playerShipWeaponSlotsLbl;
+
+	@FXML
+	private Label shipyardShipFuelLbl;
+
+	@FXML
+	private Label playerShipCargoSizeLbl;
+
+	@FXML
+	private Label shipyardShipWeaponSlotsLbl;
+
+	@FXML
+	private Label shipyardShipCrewSpaceLbl;
+
+	@FXML
+	private Label playerShipCrewSpaceLbl;
+
+	@FXML
+	private ComboBox<?> shipDropDown;
+
+	@FXML
+	private Label playerShipHPLbl;
+
+	@FXML
+	void selectShip(Event event) {
+
+	}
+
+	@FXML
+	void buyShipFromShipyard(Event event) {
+
+	}
+
+}
