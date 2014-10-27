ab2011817192e4e6bf861405639b82eb52460dd4
Jack Croft <jackcroft2@yahoo.com>
2014-10-26 21:48:59 -0400

    Cleanup and added a back to planet button in the shipyard

--- src/main/java/view/ShipyardScreenController.java
+++ src/main/java/view/ShipyardScreenController.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,14 +9,15 @@ import org.controlsfx.dialog.Dialogs;
 import controller.GameEngine;
 import model.Player;
 import model.Enum.ShipType;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
 /**
@@ -95,6 +97,9 @@ public class ShipyardScreenController {
 	@FXML
 	private Label playerShipHPLbl;
 
+	@FXML
+	private Button backToPlanetBtn;
+
 	/**
 	 * Method for selecting a new ship from the drop down menu
 	 * 
@@ -125,6 +130,31 @@ public class ShipyardScreenController {
 				for (String error : errors)
 					errorMsg += error + "\n";
 				displayError(errorMsg);
+			}
+		}
+	}
+
+	/**
+	 * Sends player back to the planet screen from the market screen
+	 * 
+	 * @param e
+	 *            The event that fired the method
+	 */
+	@FXML
+	private void toPlanetScreen(Event e) {
+		if (MultiPageController.isValidAction(e)) {
+			try {
+				Stage stage = (Stage) backToPlanetBtn.getScene().getWindow();
+				stage.hide();
+				FXMLLoader loader = new FXMLLoader(
+						ClassLoader.getSystemResource("view/PlanetScreen.fxml"));
+				Parent newScene = loader.load();
+				stage.setScene(new Scene(newScene, 600, 400));
+				PlanetScreenController controller = loader.getController();
+				controller.initializePage();
+				stage.show();
+			} catch (IOException ie) {
+				ie.printStackTrace();
 			}
 		}
 	}
