2ed8f7af996c46ea0f78ab63c7f2b1b318c9ca78
Hemen Shah <hemenshah30@gatech.edu>
2014-11-13 22:46:30 -0500

    refixing code stuff seems to be happening we may be pushing too fast

--- src/test/java/controller/ShipyardEngineTest.java
+++ src/test/java/controller/ShipyardEngineTest.java
@@ -32,175 +32,175 @@ import javafx.scene.paint.Color;
  */
 public class ShipyardEngineTest {
 
-	Planet planet;
-	Ship ship;
-	Player player;
-	ShipyardEngine engine;
-	Police police;
-	NPCEncounter encounter;
-	Location loc;
-	// These are ship types that are expected on a planet given a tech level.
-	// These arrays may need to be changed if more ship types are added or if
-	// values within certain shiptypes change
-	ShipType expectedShipTypesTechLevel0[] = {};
-	ShipType expectedShipTypesTechLevel1[] = {};
-	ShipType expectedShipTypesTechLevel2[] = {};
-	ShipType expectedShipTypesTechLevel3[] = {};
-	ShipType expectedShipTypesTechLevel4[] = { ShipType.FLEA };
-	ShipType expectedShipTypesTechLevel5[] = { ShipType.FLEA, ShipType.GNAT,
-			ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
-			ShipType.BEETLE };
-	ShipType expectedShipTypesTechLevel6[] = { ShipType.FLEA, ShipType.GNAT,
-			ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
-			ShipType.BEETLE, ShipType.HORNET, ShipType.GRASSHOPPER,
-			ShipType.TERMITE };
-	ShipType expectedShipTypesTechLevel7[] = { ShipType.FLEA, ShipType.GNAT,
-			ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
-			ShipType.BEETLE, ShipType.HORNET, ShipType.GRASSHOPPER,
-			ShipType.TERMITE, ShipType.WASP, ShipType.MANTIS };
+    Planet planet;
+    Ship ship;
+    Player player;
+    ShipyardEngine engine;
+    Police police;
+    NPCEncounter encounter;
+    Location loc;
+    // These are ship types that are expected on a planet given a tech level.
+    // These arrays may need to be changed if more ship types are added or if
+    // values within certain shiptypes change
+    ShipType[] expectedShipTypesTechLevel0 = {};
+    ShipType[] expectedShipTypesTechLevel1 = {};
+    ShipType[] expectedShipTypesTechLevel2 = {};
+    ShipType[] expectedShipTypesTechLevel3 = {};
+    ShipType[] expectedShipTypesTechLevel4 = { ShipType.FLEA };
+    ShipType[] expectedShipTypesTechLevel5 = { ShipType.FLEA, ShipType.GNAT,
+            ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
+            ShipType.BEETLE };
+    ShipType[] expectedShipTypesTechLevel6 = { ShipType.FLEA, ShipType.GNAT,
+            ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
+            ShipType.BEETLE, ShipType.HORNET, ShipType.GRASSHOPPER,
+            ShipType.TERMITE };
+    ShipType[] expectedShipTypesTechLevel7 = { ShipType.FLEA, ShipType.GNAT,
+            ShipType.FIREFLY, ShipType.MOSQUITO, ShipType.BUMBLEBEE,
+            ShipType.BEETLE, ShipType.HORNET, ShipType.GRASSHOPPER,
+            ShipType.TERMITE, ShipType.WASP, ShipType.MANTIS };
 
-	@Before
-	public void setUp() throws Exception {
-		loc = new Location(50, 50);
-		ship = new Ship(ShipType.FLEA);
-		player = new Player("Test", 5, 5, 5, 5, 5, ship);
-		engine = new ShipyardEngine(player);
-	}
+    @Before
+    public void setUp() throws Exception {
+        loc = new Location(50, 50);
+        ship = new Ship(ShipType.FLEA);
+        player = new Player("Test", 5, 5, 5, 5, 5, ship);
+        engine = new ShipyardEngine(player);
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel0() {
-		planet = new Planet("test", TechLevel.PREAGRICULTURE,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel0() {
+        planet = new Planet("test", TechLevel.PREAGRICULTURE,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel0.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel0) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel0.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel0) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel1() {
-		planet = new Planet("test", TechLevel.AGRICULTURE,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel1() {
+        planet = new Planet("test", TechLevel.AGRICULTURE,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel1.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel1) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel1.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel1) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel2() {
-		planet = new Planet("test", TechLevel.MEDIEVAL, SpecialResource.DESERT,
-				Government.DEMOCRACY, loc, Condition.BOREDOM,
-				EncounterRate.FEW, EncounterRate.FEW, EncounterRate.FEW, 1,
-				Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel2() {
+        planet = new Planet("test", TechLevel.MEDIEVAL, SpecialResource.DESERT,
+                Government.DEMOCRACY, loc, Condition.BOREDOM,
+                EncounterRate.FEW, EncounterRate.FEW, EncounterRate.FEW, 1,
+                Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel2.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel2) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel2.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel2) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel3() {
-		planet = new Planet("test", TechLevel.RENAISSANCE,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel3() {
+        planet = new Planet("test", TechLevel.RENAISSANCE,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel3.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel3) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel3.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel3) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel4() {
-		planet = new Planet("test", TechLevel.EARLYINDUSTRIAL,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel4() {
+        planet = new Planet("test", TechLevel.EARLYINDUSTRIAL,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel4.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel4) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel4.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel4) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel5() {
-		planet = new Planet("test", TechLevel.INDUSTRIAL,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel5() {
+        planet = new Planet("test", TechLevel.INDUSTRIAL,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel5.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel5) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel5.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel5) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel6() {
-		planet = new Planet("test", TechLevel.POSTINDUSTRIAL,
-				SpecialResource.DESERT, Government.DEMOCRACY, loc,
-				Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
-				EncounterRate.FEW, 1, Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel6() {
+        planet = new Planet("test", TechLevel.POSTINDUSTRIAL,
+                SpecialResource.DESERT, Government.DEMOCRACY, loc,
+                Condition.BOREDOM, EncounterRate.FEW, EncounterRate.FEW,
+                EncounterRate.FEW, 1, Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel6.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel6) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel6.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel6) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
-	@Test
-	public void getAvailableShipsTestTechLevel7() {
-		planet = new Planet("test", TechLevel.HITECH, SpecialResource.DESERT,
-				Government.DEMOCRACY, loc, Condition.BOREDOM,
-				EncounterRate.FEW, EncounterRate.FEW, EncounterRate.FEW, 1,
-				Color.AQUA);
-		player.setPlanet(planet);
+    @Test
+    public void getAvailableShipsTestTechLevel7() {
+        planet = new Planet("test", TechLevel.HITECH, SpecialResource.DESERT,
+                Government.DEMOCRACY, loc, Condition.BOREDOM,
+                EncounterRate.FEW, EncounterRate.FEW, EncounterRate.FEW, 1,
+                Color.AQUA);
+        player.setPlanet(planet);
 
-		List<ShipType> available = engine.getAvailableShips();
-		assertEquals("Number of ships dont match",
-				expectedShipTypesTechLevel7.length, available.size());
-		for (ShipType expected : expectedShipTypesTechLevel7) {
-			assertTrue("At least one of the expected ships is not returned",
-					available.contains(expected));
-		}
-	}
+        List<ShipType> available = engine.getAvailableShips();
+        assertEquals("Number of ships dont match",
+                expectedShipTypesTechLevel7.length, available.size());
+        for (ShipType expected : expectedShipTypesTechLevel7) {
+            assertTrue("At least one of the expected ships is not returned",
+                    available.contains(expected));
+        }
+    }
 
 }
