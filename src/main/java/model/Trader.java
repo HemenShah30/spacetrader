0cae63cfb01daa4b3ddfdadef384e14155d5243b
Eric Wan <eric4wan@gmail.com>
2014-11-13 22:03:17 -0500

    Actually un-red-ed now

--- src/main/java/model/Trader.java
+++ src/main/java/model/Trader.java
@@ -12,7 +12,7 @@ import java.util.Random;
  * 
  */
 public class Trader extends NPC {
-    private static final double percentBuying = 0.5;
+    private static final double PERCENTBUYING = 0.5;
     private int quantity;
     private int price;
     private boolean isBuying; // determines if the Trader wants to sell
@@ -29,7 +29,7 @@ public class Trader extends NPC {
      *            The TraderReputation for the player
      */
     public Trader(int rep) {
-        this(rep, (Math.random() < percentBuying));
+        this(rep, (Math.random() < PERCENTBUYING));
     }
 
     /**
