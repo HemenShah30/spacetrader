ae0246fd7d168f254414e54befb481347a111f52
Caroline Zhang <czhang363@gatech.edu>
2014-12-01 18:48:07 -0500

    The icon I added forever ago but never committed

--- src/main/java/spaceTrader/Main.java
+++ src/main/java/spaceTrader/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -26,6 +27,7 @@ public class Main extends Application {
                 .getSystemResourceAsStream("view/StartScreen.fxml"));
         primaryStage.setTitle("Space Traders");
         primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.getIcons().add(new Image("file:resources/spaceTraderIcon.png"));
         primaryStage.show();
     }
 
